5389d1b834aac34b8a9bdb4a62703d8c9bcfdf68
wbrophy <2799601+wbrophy@users.noreply.github.com>
2024-05-22 16:00:22 -0400

    Add files via upload

--- ESP8266ServerFoundation_I2C.ino
+++ ESP8266ServerFoundation_I2C.ino
@@ -628,13 +628,13 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
     switch (type)
       {
         case WStype_DISCONNECTED:
-          Serial.printf("[%u] Disconnected!\n", num);
+          Serial.printf("[%u] WebSocket Disconnected!\n", num);
           break;
 
         case WStype_CONNECTED: 
           {
             IPAddress ip = webSocket.remoteIP(num);
-            Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+            Serial.printf("[%u] WebSocket Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
             // send message to client
             webSocket.sendTXT(num, "0");
           }
