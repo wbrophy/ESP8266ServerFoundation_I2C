c2870dcb40ee8fb87ae76b1b5ccb64f568835299
wbrophy <2799601+wbrophy@users.noreply.github.com>
2024-08-30 01:46:55 -0400

    Add files via upload

--- i2c_device_files/BME280/76.js
+++ i2c_device_files/BME280/76.js
@@ -26,32 +26,6 @@ socket.onmessage = function(e)
   let x_var = JSON.parse("{}");
   let x_data = JSON.parse(e.data);
   let x_device = x_data.i2cbus.device.filter(function (entry) { return entry.address == x_strdeviceId; });
-  /*
-  x_var["x"] = TwosCompliment16((x_device[0].getdata[0].data[1] << 8 | x_device[0].getdata[0].data[0]));
-  x_var["z"] = TwosCompliment16((x_device[0].getdata[0].data[3] << 8 | x_device[0].getdata[0].data[2]));
-  x_var["y"] = TwosCompliment16((x_device[0].getdata[0].data[5] << 8 | x_device[0].getdata[0].data[4]));
-  x_averageX.shift();
-  x_averageX.push(x_var["x"]);
-  x_averageY.shift();
-  x_averageY.push(x_var["y"]);
-  x_averageZ.shift();
-  x_averageZ.push(x_var["z"]);
-  let loopx = 0;
-  for (x_loop = 0; x_loop < x_averageX.length; x_loop++)
-   { loopx += x_averageX[x_loop]; }
-  x_xaxis = loopx / x_averageX.length;
-  let loopy = 0;
-  for (x_loop = 0; x_loop < x_averageY.length; x_loop++)
-   { loopy += x_averageY[x_loop]; }
-  x_yaxis = loopy / x_averageY.length;
-  let loopz = 0;
-  for (x_loop = 0; x_loop < x_averageZ.length; x_loop++)
-   { loopz += x_averageZ[x_loop]; }
-  x_zaxis = loopz / x_averageZ.length;
 
-  document.getElementById('x_var_data').innerHTML = JSON.stringify(x_var);
-  document.getElementById('x_device').innerHTML = JSON.stringify(x_device);
-  document.getElementById('x_device').innerHTML = '<br>X: ' + String(x_xaxis.toFixed(0)) + '<br>Y: ' + String(x_yaxis.toFixed(0)) + '<br>Z: ' + String(x_zaxis.toFixed(0));
-  */
   document.getElementById('raw_data').innerHTML = e.data;
  };
